25d48c2e82dc6c6c02ac9084f1b1b8ad129661cc
Hung Nguyen <nguyenhung@tma.com.vn>
2024-11-28 10:27:22 +0700

    Add check install python package

--- eventyay_paypal/apps.py
+++ eventyay_paypal/apps.py
@@ -3,16 +3,23 @@ from django.utils.translation import gettext_lazy as _
 
 from . import __version__
 
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Python package 'paypal' is not installed.")
+
+
 class PaypalPluginApp(AppConfig):
     default = True
-    name = 'eventyay_paypal'
+    name = "eventyay_paypal"
     verbose_name = _("PayPal")
 
     class PretixPluginMeta:
         name = _("PayPal")
         author = "eventyay"
         version = __version__
-        category = 'PAYMENT'
+        category = "PAYMENT"
         featured = True
         visible = True
         description = _("This plugin allows you to receive payments via PayPal.")
@@ -21,4 +28,4 @@ class PaypalPluginApp(AppConfig):
         from . import signals  # NOQA
 
 
-default_app_config = 'eventyay-paypal.apps.PaypalPluginApp'
+default_app_config = "eventyay-paypal.apps.PaypalPluginApp"
